5c411f4d54d27d7f2536322b40861f71772df8cd
Sophia Koeut <koeutsophia@gmail.com>
2022-06-28 22:28:41 -0700

    decreased padding for gift section

--- src/components/gift/index.js
+++ src/components/gift/index.js
@@ -9,7 +9,7 @@ class Gift extends Component {
         <div className="container">
           <div className="col-12">
             <div className="section-title text-center">
-              <h2>Gift Registration</h2>
+              <h2>Gift Registrations</h2>
               <p>We have chosen to use our gift registry through Blueprint to fund our honeymoon experience. You are welcome to select any item to make a contribution. We appreciate your support and help in making our honeymoon a magical experience.</p>
             </div>
           </div>
