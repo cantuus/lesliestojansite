3f55cb39dc3e56fc29058d6f994c7604035bb1c3
Cory Stojan <cory.stojan@gmail.com>
2022-06-28 21:05:49 -0700

    Fixed the countdown timmer

--- src/components/countdown/index.js
+++ src/components/countdown/index.js
@@ -17,7 +17,7 @@ class Saveday extends Component {
                                 </div>
                                 <div className="col-12 col-md-8 clock-area">
                                     <div className="count-down-clock">
-                                    <DateCountdown dateTo='July 023, 2022 15:00:00 GMT-08:00' mostSignificantFigure='day'/>
+                                    <DateCountdown dateTo='July 023, 2022 15:00:00 GMT-07:00' mostSignificantFigure='day'/>
                                     </div>
                                 </div>
                             </div>
